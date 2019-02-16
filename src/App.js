1c29ce027d0f8235b546fb83f013662f501e64e6
Chester Hansen <chesterehansen@gmail.com>
2019-02-16 14:26:44 -0700

    added calculator component to app

--- src/App.js
+++ src/App.js
@@ -67,6 +67,42 @@ class Keylogger extends Component {
   }
 }
 
+
+class Calculator extends Component{
+  state={
+    num1: 0,
+    num2: 0,
+    total: 0
+  }
+
+  handleChangeNum1 = (element) => this.setState({
+    num1 : parseInt(element.target.value)
+  })
+
+  handleChangeNum2 = (element) => this.setState({
+    num2 : parseInt(element.target.value)
+  })
+
+  addTheNumbers = () => this.setState({
+    total: this.state.num1 + this.state.num2
+  })
+
+  render() {
+    return(
+      <div className='Calc'>
+        <h1>Add Two Numbers</h1>
+        Num 1: <input onChange={this.handleChangeNum1} type="text"/>
+        <br/>
+        Num 1: <input onChange={this.handleChangeNum2} type="text"/>
+        <br/>
+        <button onClick={this.addTheNumbers}>Add</button>
+        <br/>
+        {this.state.total}
+      </div>
+    );
+  }
+}
+
 // class Codelist extends Component {
 //   state= {
 //     listvalues: ['Nothing in your To-do list']
