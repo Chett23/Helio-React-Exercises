888fdcc653aab585e94f02ae7b947ee2c0c8e748
Chester Hansen <chesterehansen@gmail.com>
2023-11-07 11:57:56 -0700

    updated dependancies and changed to new react-router standards

--- src/App.js
+++ src/App.js
@@ -1,109 +1,139 @@
-import React, { Component } from 'react';
-import {
-  BrowserRouter as Router,
-  Route,
-  NavLink,
-  Switch
-} from 'react-router-dom';
-import Helmet from './Components/Helmet'
-import Keylogger from './Pages/Keylogger';
-import Codelist from './Pages/Codelist';
-import Calculator from './Pages/Calculator';
-import Tacos from './Pages/Tacos';
-import NotFound from './Pages/NotFound';
-import Testing from './Pages/Testing';
+import React from "react";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+import Helmet from "./Components/Helmet";
+import { default as Todo } from "./Pages/Keylogger";
+import Codelist from "./Pages/Codelist";
+import Calculator from "./Pages/Calculator";
+import Tacos from "./Pages/Tacos";
+import NotFound from "./Pages/NotFound";
+import Testing from "./Pages/Testing";
 
+const App = () => {
+	return (
+		<div>
+			<Helmet />
+			<div>
+				<ul
+					style={{
+						listStyle: "none",
+						display: "inline-grid",
+						gridTemplateColumns:
+							"10px auto 100px 100px 100px 100px 100px 10px",
+						width: "100%",
+					}}
+				>
+					<h1
+						style={{
+							gridColumnStart: "2",
+							gridColumnEnd: "span 1",
+						}}
+					>
+						Helio Training React Exercises
+					</h1>
+					<li>
+						<a
+							href={"/"}
+							style={{
+								gridColumnStart: "3",
+								gridColumnEnd: "span 1",
+								textDecoration: "none",
+								color: "black",
+							}}
+						>
+							Home
+						</a>
+					</li>
+					<li>
+						<a
+							href={"/todo"}
+							style={{
+								gridColumnStart: "4",
+								gridColumnEnd: "span 1",
+								textDecoration: "none",
+								color: "black",
+							}}
+						>
+							Todo
+						</a>
+					</li>
+					<li>
+						<a
+							href={"/Calculator"}
+							style={{
+								gridColumnStart: "5",
+								gridColumnEnd: "span 1",
+								textDecoration: "none",
+								color: "black",
+							}}
+						>
+							Calculator
+						</a>
+					</li>
+					<li>
+						<a
+							href={"/CodeList"}
+							style={{
+								gridColumnStart: "6",
+								gridColumnEnd: "span 1",
+								textDecoration: "none",
+								color: "black",
+							}}
+						>
+							Code List
+						</a>
+					</li>
+					<li>
+						<a
+							href={"/Testing"}
+							style={{
+								gridColumnStart: "6",
+								gridColumnEnd: "span 1",
+								textDecoration: "none",
+								color: "black",
+							}}
+						>
+							Testing
+						</a>
+					</li>
+				</ul>
+			</div>
+			<Outlet />
+		</div>
+	);
+};
 
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <App />,
+		errorElement: <NotFound />,
+		children: [
+			{
+				path: "/todo",
+				element: <Todo />,
+			},
+			{
+				path: "/calculator",
+				element: <Calculator />,
+			},
+			{
+				path: "/codelist",
+				element: <Codelist />,
+			},
+			{
+				path: "/testing",
+				element: <Testing />,
+			},
+			{
+				path: "/tacos",
+				element: <Tacos />,
+			},
+		],
+	},
+]);
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <Helmet />
-        <Router>
-          <div>
-            <ul 
-              style={{
-                listStyle: 'none', 
-                display: 'inline-grid',
-                gridTemplateColumns: '10px auto 100px 100px 100px 100px 100px 10px',
-                width:'100%'
-                }}>
-              <h1 style={{
-                    gridColumnStart: '2',
-                    gridColumnEnd: 'span 1'
-                  }}
-                  >Helio Training React Exercises</h1>
-              <li>
-                <NavLink
-                  to='/' 
-                  style={{
-                    gridColumnStart: '3',
-                    gridColumnEnd: 'span 1',
-                    textDecoration: 'none',
-                    color: 'black'
-                  }}
-                >Home</NavLink></li>
-              <li>
-                <NavLink
-                  to='/Keylogger' 
-                  style={{
-                    gridColumnStart: '4',
-                    gridColumnEnd: 'span 1',
-                    textDecoration: 'none',
-                    color: 'black'
-                  }}
-                >Keylogger</NavLink></li>
-              <li>
-                <NavLink
-                  to='/Calculator' 
-                  style={{
-                    gridColumnStart: '5',
-                    gridColumnEnd: 'span 1',
-                    textDecoration: 'none',
-                    color: 'black'
-                  }}
-                >Calculator</NavLink></li>
-              <li>
-                <NavLink
-                  to='/CodeList' 
-                  style={{
-                    gridColumnStart: '6',
-                    gridColumnEnd: 'span 1',
-                    textDecoration: 'none',
-                    color: 'black'
-                  }}
-                >Code List</NavLink></li>
-                <li>
-                  <NavLink
-                    to='/Testing' 
-                    style={{
-                      gridColumnStart: '6',
-                      gridColumnEnd: 'span 1',
-                      textDecoration: 'none',
-                      color: 'black'
-                    }}
-                  >Testing</NavLink></li>
-            </ul>
-            <Switch> 
-              <Route exact path="/" component={()=>(
-                <div style={{ textAlign:'center' }}>
-                  <hr/>
-                  <h1>Home</h1>
-                </div>
-              )} />
-              <Route path="/Keylogger" component={Keylogger} />
-              <Route path="/Calculator" component={Calculator} />
-              <Route path="/CodeList" component={Codelist} />
-              <Route path="/Testing" component={Testing} />
-              <Route path="/Tacos" component={Tacos} />
-              <Route component={NotFound} />
-            </Switch>
-          </div>
-        </Router>
-      </div>
-    );
-  }
-}
+const RouterComponent = () => {
+	return <RouterProvider router={router} />;
+};
 
-export default App;
+export default RouterComponent;
